5d4bc5e262775a4842a6d0f4c5339915ce79bd0f
bryguy82 <and18017@byui.edu>
2018-11-06 01:45:30 -0200

    feedback adjustments

---
+++ assignments/lesson-8/scripts/slider.js
@@ -0,0 +1,8 @@
+
+    function printValue(sliderID, textbox) {
+        var x = document.getElementById(textbox);
+        var y = document.getElementById(sliderID);
+        x.value = y.value;
+    }
+
+    window.onload = function() { printValue('range', 'rangeValue'); }
