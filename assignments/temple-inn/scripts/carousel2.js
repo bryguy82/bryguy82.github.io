67a7f3412e913f737849e171da4f69770724c354
bryguy82 <and18017@byui.edu>
2018-12-10 22:40:37 -0200

    weather

--- assignments/temple-inn/scripts/carousel2.js
+++ assignments/temple-inn/scripts/carousel2.js
@@ -1,23 +1,66 @@
 
-var index = 1;
-changeSlides(index);
+$(function () {
 
-function moveSlides(n) {
-    changeSlides(index += n);
-}
+    var box1 = $(".temple-info1");
+    var box2 = $(".temple-info2");
+    var box3 = $(".temple-info3");
+    var box4 = $(".temple-info4");
 
-function changeSlides(n){
-    var images = document.getElementsByClassName("temple-info");
-
-    if (n > images.length) {
-        index = 1;
+    function left1() {
+        box1.css("display", "none");
+        box4.addClass("show-temple");        
     }
-    if (n < 1) {
-        index = images.length;
+
+    $(".click-left1").click(left1);
+
+    function left4() {
+        box4.removeClass("show-temple");
+        box3.addClass("show-temple");
     }
-    for (i = 0; i < images.length; i++) {
-        images[i].style.display = "none";
 
+    $(".click-left4").click(left4);
 
-    images[index - 1].style.display = "grid";}
-}
+    function left3() {
+        box3.removeClass("show-temple");
+        box2.addClass("show-temple");
+    }
+
+    $(".click-left3").click(left3);
+
+    function left2() {
+        box2.removeClass("show-temple");
+        box1.css("display", "grid");
+    }
+
+    $(".click-left2").click(left2);
+/*-------------------------------------------*/
+
+    function right1() {
+        box1.css("display", "none");
+        box2.addClass("show-temple");        
+    }
+
+    $(".click-right1").click(right1);
+
+    function right2() {
+        box2.removeClass("show-temple");
+        box3.addClass("show-temple");
+    }
+
+    $(".click-right2").click(right2);
+
+    function right3() {
+        box3.removeClass("show-temple");
+        box4.addClass("show-temple");
+    }
+
+    $(".click-right3").click(right3);
+
+    function right4() {
+        box4.removeClass("show-temple");
+        box1.css("display", "grid");
+    }
+
+    $(".click-right4").click(right4);
+
+})
